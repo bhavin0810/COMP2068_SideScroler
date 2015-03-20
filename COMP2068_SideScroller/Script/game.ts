3ac61ecc9f975eeada9734b04f16d06d36d71efd
bhavin0810 <bhavin0810@gmail.com>
2015-03-20 03:38:16 -0400

    Game Objects Added

--- COMP2068_SideScroller/Script/game.ts
+++ COMP2068_SideScroller/Script/game.ts
@@ -1,7 +1,49 @@
 ﻿/// <reference path="typings/createjs-lib/createjs-lib.d.ts" />
+/// <reference path="typings/easeljs/easeljs.d.ts" />
+/// <reference path="typings/tweenjs/tweenjs.d.ts" />
+/// <reference path="typings/soundjs/soundjs.d.ts" />
+/// <reference path="typings/preloadjs/preloadjs.d.ts" />
+/// <reference path="typings/stats/stats.d.ts" />
 
+
+/// <reference path="constants.ts" />
+/// <reference path="objects/gameobject.ts" />
+/// <reference path="objects/space.ts" />
+/// <reference path="objects/spaceshuttle.ts" />
+/// <reference path="objects/smallinsect.ts" />
+/// <reference path="objects/biginsect.ts" />
+
+
+
+// Game Variables +++++++++++++++++++++++++++++++++++++++++++++
+var stats: Stats = new Stats();
 var canvas;
 var stage: createjs.Stage;
+var assetLoader: createjs.LoadQueue;
+var game: createjs.Container;
+
+// Game Objects
+var space: objects.Space;
+var spaceShuttle: objects.SpaceShuttle;
+var smallInsect: objects.SmallInsect[] = [];
+var bigInsect: objects.BigInsect[] = [];
+
+// asset manifest - array of asset objects
+var manifest = [
+    { id: "spaceShuttle", src: "assets/images/spaceShuttle.jpg" },
+    { id: "space", src: "assets/images/space.jpg" },
+    { id: "smallInsects", src: "assets/images/smallInsects.png" },
+    { id: "bigInsects", src: "assets/images/bigInsects.png" },    
+];
+
+
+function preload() {
+    assetLoader = new createjs.LoadQueue(); // instantiated assetLoader
+    assetLoader.installPlugin(createjs.Sound);
+    assetLoader.on("complete", init, this); // event handler-triggers when loading done
+    assetLoader.loadManifest(manifest); // loading my asset manifest
+}
+
 
 function init() {
     canvas = document.getElementById("canvas");
@@ -9,17 +51,67 @@ function init() {
     stage.enableMouseOver(20); // Enable mouse events
     createjs.Ticker.setFPS(60); // 60 frames per second
     createjs.Ticker.addEventListener("tick", gameLoop);
+    setupStats();
 
     main();
 }
 
-function gameLoop() {
-    stage.update(); // Refreshes our stage
+// UTILITY METHODS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function setupStats() {
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '650px';
+    stats.domElement.style.top = '440px';
+    document.body.appendChild(stats.domElement);
 }
 
+//GAME LOOP +++++++++++++++++++++++++++++
+function gameLoop() {
+    stats.begin(); // Begin metering
+    space.update();
+    spaceShuttle.update();
 
+    for (var insect = constants.SMALLINSECTS_NUM; insect> 0; insect--) {
+        smallInsect[insect].update();
+        
+    }
+
+    for (var insect = constants.BIGINSECTS_NUM; insect > 0; insect--) {
+        bigInsect[insect].update();
+
+    }
+
+    stage.update(); // Refreshes our stage
+
+    stats.end(); // End metering
+}
 
 // Our Game Kicks off in here
 function main() {
+    // Instantiate Game Container
+    game = new createjs.Container();
+
+    // Add space to game
+    space = new objects.Space();
+    game.addChild(space);
+
+
+    // Add island to game
+    spaceShuttle= new objects.SpaceShuttle();
+    game.addChild(spaceShuttle);
+
+  
+
+    for (var insect = constants.SMALLINSECTS_NUM; insect > 0; insect--) {
+        smallInsect[insect] = new objects.SmallInsect();
+        game.addChild(smallInsect[insect]);
+    }
+
+    for (var insect = constants.BIGINSECTS_NUM; insect > 0; insect--) {
+        bigInsect[insect] = new objects.BigInsect();
+        game.addChild(bigInsect[insect]);
+    }
+
+    stage.addChild(game);
 
 }
---
+++ COMP2068_SideScroller/Script/objects/smallInsect.ts
@@ -0,0 +1,44 @@
+﻿module objects {
+
+    export class SmallInsect extends objects.GameObject {
+
+        // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++
+        constructor() {
+            super("smallInsects");
+            this.name = "smallInsects";
+            //this.soundString = "thunder";
+
+            this._reset();
+
+        }
+
+        // PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++
+        // PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++
+        private _reset() {
+            // set the island to start at a random x value
+            this.y = Math.floor(Math.random() * constants.SCREEN_HEIGHT);
+            this.x = 0;
+            // add drift to the cloud 
+            this._dx = Math.floor(Math.random() * 5) + 5;
+            this._dy = Math.floor(Math.random() * 4) - 2;
+        }
+
+        private _checkBounds() {
+            if (this.x > (constants.SCREEN_WIDTH + this.width)) {
+                this._reset();
+            }
+        }
+
+
+        // PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++
+
+        public update() {
+            this.y -= this._dy;
+            this.x += this._dx;
+
+
+            this._checkBounds();
+        }
+
+    }
+}
