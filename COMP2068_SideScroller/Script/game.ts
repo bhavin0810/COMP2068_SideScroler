158434eb70a7eff1458125bda67c99602577ec82
bhavin0810 <bhavin0810@gmail.com>
2015-03-19 22:45:46 -0400

    Createjs Library references added to the index page

---
+++ COMP2068_SideScroller/Script/game.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="typings/createjs-lib/createjs-lib.d.ts" />
+
+var canvas;
+var stage: createjs.Stage;
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20); // Enable mouse events
+    createjs.Ticker.setFPS(60); // 60 frames per second
+    createjs.Ticker.addEventListener("tick", gameLoop);
+
+    main();
+}
+
+function gameLoop() {
+    stage.update(); // Refreshes our stage
+}
+
+
+
+// Our Game Kicks off in here
+function main() {
+
+}
